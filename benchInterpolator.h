efc8efc22fa9ddec0167e4afb9b663c250a6783c
Rupert Brooks <rupert@morrigan.(none)>
2013-08-13 14:11:57 -0400

    Linux compile fix

--- benchInterpolator.h
+++ benchInterpolator.h
@@ -35,19 +35,19 @@ public:
       itk::MultiThreader::SetGlobalDefaultNumberOfThreads(numthreads);
       
       typedef itk::RandomImageSource<ImageType> SourceType;
-      SourceType::Pointer s1=SourceType::New();
-      ImageType::SizeType sz;
+      typename SourceType::Pointer s1=SourceType::New();
+      typename ImageType::SizeType sz;
       sz[0]=50;
       sz[1]=60;
       sz[2]=70;
 
-      ImageType::SpacingType spacing;
+      typename ImageType::SpacingType spacing;
       spacing[0]=0.567;spacing[1]=0.789;spacing[2]=1.234;
-      ImageType::PointType origin;
+      typename ImageType::PointType origin;
       origin[0]=-12.3;origin[1]=23.4;origin[2]=-34.5;
       vnl_vector_fixed<double,3> axis;
       axis[0]=0.1; axis[1]=0.2; axis[2]=0.3;
-      ImageType::DirectionType direction(vnl_rotation_matrix(axis));
+      typename ImageType::DirectionType direction(vnl_rotation_matrix(axis));
       s1->SetSpacing(spacing);
       s1->SetOrigin(origin);
       //      s1->SetDirection(direction);
@@ -76,9 +76,9 @@ public:
    const char * GetNameOfBenchmark() const {return m_Name.c_str();}
 
    void operator()() const {
-      ImageType::SizeType sz=m_Image->GetLargestPossibleRegion().GetSize();
-      ImageType::IndexType ix; ix.Fill(0);
-      ImageType::PointType pt1,pt2,pt;
+      typename ImageType::SizeType sz=m_Image->GetLargestPossibleRegion().GetSize();
+      typename ImageType::IndexType ix; ix.Fill(0);
+      typename ImageType::PointType pt1,pt2,pt;
       m_Image->TransformIndexToPhysicalPoint(ix,pt1);
       for(unsigned int d=0;d<ImageType::GetImageDimension();++d) {
          ix[d]=sz[d];
@@ -88,7 +88,7 @@ public:
          for(pt[1]=std::min(pt1[1],pt2[1]);pt[1]<std::max(pt1[1],pt2[1]);pt[1]+=1.2334) {
             for(pt[2]=std::min(pt1[2],pt2[2]);pt[2]<std::max(pt1[2],pt2[2]);pt[2]+=1.2334) {
                if(m_Interpolator->IsInsideBuffer(pt)) {
-                  ImageType::PixelType p=m_Interpolator->Evaluate(pt);
+                  typename ImageType::PixelType p=m_Interpolator->Evaluate(pt);
                }
             }
          }
